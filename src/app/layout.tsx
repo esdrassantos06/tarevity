803fc9ce0e4ae887d437bca9112f1a021ccab99f
esdrassantos06 <esdrasirion1@gmail.com>
2025-03-13 00:27:16 +0000

    feat: Add favicon and manifest for PWA support
    This commit adds a favicon and a manifest file to enable Progressive Web App (PWA) support.
    
    -   **favicon.ico:** Added the favicon file to the project.
    -   **manifest.json:** Created a manifest file to define the PWA's metadata, including name, icons, theme color, and start URL.
    -   **layout.tsx:** Updated the `layout.tsx` file to include the necessary meta tags for the favicon and manifest, and added apple touch icons.
    -   **Favicon files:** Added various sizes of apple touch icons and favicon png files.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,32 @@ export const metadata: Metadata = {
   creator: 'Esdras Santos',
   publisher: 'Tarevity',
   robots: 'index, follow',
+  icons: {
+    icon: [
+      { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/favicon/apple-touch-icon-57x57.png', sizes: '57x57', type: 'image/png' },
+      { url: '/favicon/apple-touch-icon-60x60.png', sizes: '60x60', type: 'image/png' },
+      { url: '/favicon/apple-touch-icon-72x72.png', sizes: '72x72', type: 'image/png' },
+      { url: '/favicon/apple-touch-icon-76x76.png', sizes: '76x76', type: 'image/png' },
+      { url: '/favicon/apple-touch-icon-114x114.png', sizes: '114x114', type: 'image/png' },
+      { url: '/favicon/apple-touch-icon-120x120.png', sizes: '120x120', type: 'image/png' },
+      { url: '/favicon/apple-touch-icon-144x144.png', sizes: '144x144', type: 'image/png' },
+      { url: '/favicon/apple-touch-icon-152x152.png', sizes: '152x152', type: 'image/png' },
+
+    ],
+    other: [
+      {
+        rel: 'apple-touch-icon-precomposed',
+        url: '/apple-touch-icon-precomposed.png',
+      },
+    ],
+  },
+  manifest: '/manifest.json',
 }
+
 
 export default async function RootLayout({
   children,
